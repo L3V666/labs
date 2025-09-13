513d69ceacfa3baf8fe4a6e178d500cca72727f0
Lew L3V Podmoskovnov <lev.tmb.l29@gmail.com>
2025-09-13 12:34:08 +0300

    1.1.4 13.09.2025

--- 1.1.4/1.1.4.py
+++ 1.1.4/1.1.4.py
@@ -6,65 +6,47 @@ import pandas as pd
 with open('experiment.txt', 'r') as file:
     data = np.array(list(map(int, file.readlines()[2:-2])))
 
-t10 = np.array([], dtype=int)
-t20 = np.array([], dtype=int)
-t40 = np.array([], dtype=int)
-t80 = np.array([], dtype=int)
-for i in range(0, len(data), 10):
-    t10 = np.append(t10, np.sum(data[i:i + 10]))
-for i in range(0, len(data), 20):
-    t20 = np.append(t20, np.sum(data[i:i + 20]))
-for i in range(0, len(data), 40):
-    t40 = np.append(t40, np.sum(data[i:i + 40]))
-for i in range(0, len(data), 80):
-    t80 = np.append(t80, np.sum(data[i:i + 80]))
+tau = 80
+
+t = np.array([], dtype=int)
+for i in range(0, len(data), tau):
+    t = np.append(t, np.sum(data[i:i + tau]))
 
 
-df = pd.DataFrame(t80.reshape(5, 10))
-print(t80.reshape(5, 10))
-df.to_csv('массив.csv', index=False, encoding='utf-8-sig')
+pd.DataFrame(t.reshape(5, 10)).to_csv('t80.csv', index=False, encoding='utf-8-sig')
 
 
-def w(n, t):
+def w_n(n, t):
     return np.mean(t) ** n / math.factorial(n) * np.exp(1) ** (-np.mean(t))
 
-w10 = np.array([])
-w20 = np.array([])
-w40 = np.array([])
-w80 = np.array([])
+w = np.array([])
 
-x10 = np.array([])
-x20 = np.array([])
-x40 = np.array([])
-x80 = np.array([])
+x = np.array([])
 
-for i in range(0, np.max(t10) + 1):
-    w10 = np.append(w10, w(i, t10))
-    x10 = np.append(x10, i)
-for i in range(0, np.max(t20) + 1):
-    w20 = np.append(w20, w(i, t20))
-    x20 = np.append(x20, i)
-for i in range(0, np.max(t40) + 1):
-    w40 = np.append(w40, w(i, t40))
-    x40 = np.append(x40, i)
-for i in range(0, np.max(t80) + 1):
-    x80 = np.append(x80, i)
-    w80 = np.append(w80, w(i, t80))
+for i in range(0, np.max(t) + 1):
+    w = np.append(w, w_n(i, t))
+    x = np.append(x, i)
 
-y10 = np.array([])
-y20 = np.array([])
-y40 = np.array([])
-y80 = np.array([])
+y = np.array([])
 
-for i in range(0, np.max(t10) + 1):
-    y10 = np.append(y10, np.count_nonzero(t10 == i) / 400)
-for i in range(0, np.max(t20) + 1):
-    y20 = np.append(y20, np.count_nonzero(t20 == i) / 200)
-for i in range(0, np.max(t40) + 1):
-    y40 = np.append(y40, np.count_nonzero(t40 == i) / 100)
-for i in range(0, np.max(t80) + 1):
-    y80 = np.append(y80, np.count_nonzero(t80 == i) / 50)
+for i in range(0, np.max(t) + 1):
+    y = np.append(y, np.count_nonzero(t == i) / (4000 / tau))
 
-plt.bar(x20, y20)
+plt.bar(x, y)
+plt.plot(x, w, color='red')
+
+avg_n = np.sum(t) / len(t) 
+print(f'avg_n {avg_n}')
+
+sigma_n = np.sqrt(np.sum((t - avg_n) ** 2) / len(t))
+print(f'sigma_n {sigma_n}')
+
+sigma_avg_n = sigma_n / np.sqrt(len(t))
+print(f'sigma_avg_n {sigma_avg_n}')
+
+j = avg_n / tau
+print(f'j {j}')
+
+plt.savefig('80.png')
 
 plt.show()
